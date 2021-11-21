1bb88c596811be2ad2d23807f244e117c935dc09
Tarek Massini <tarek.jannis.03.tm@gmail.com>
2021-11-21 23:00:14 +0100

    Extreme Verbesserungen dank filter und frühzeitigen Abbruchs mit `min_rating`

--- aufgabe2/aufgabe2.py
+++ aufgabe2/aufgabe2.py
@@ -1,12 +1,13 @@
 from sys import argv
-from collections import ChainMap
+from typing import Union
 
 
 class Hotel:
 
-    def __init__(self, distance: int, rating: float):
+    def __init__(self, distance: int, rating: float, parent_route):
         self.distance = distance
         self.rating = rating
+        self.parent_route = parent_route
 
     def __repr__(self) -> str:
         return "Hotel({}, {})".format(self.distance, self.rating)
@@ -14,54 +15,75 @@ class Hotel:
     def __str__(self) -> str:
         return "Position: {}, Bewertung: {}".format(self.distance, self.rating)
 
+    def __lt__(self, other):
+        assert type(other) == Hotel
+        return self.rating < other.rating
+
+    @property
+    def daily_range(self):
+        return self.parent_route.range_per_day(self)
+
 
 class Route:
 
     def __init__(self, path: str) -> None:
+        self.min_rating = None
         self.hotels = []
-        self.position = 0
         with open(path, "r") as file:
             hotel_amount = int(file.readline())
             self.total_distance = int(file.readline())
             for hotel in range(hotel_amount):
                 distance, rating = file.readline().split(" ")
-                self.hotels.append(Hotel(int(distance), float(rating)))
+                self.hotels.append(Hotel(int(distance), float(rating), self))
 
     def __str__(self) -> str:
-        output = ""
-        for index, hotel in enumerate(self.find_route()):
-            output += "{}. {}\n".format(index + 1, hotel)
+        possible_routes = self.find_route()
+        if possible_routes:
+            output = ""
+            for index, hotel in enumerate(possible_routes):
+                output += "{}. {}\n".format(index + 1, hotel)
+        else:
+            output = "Es wurde keine Lösung gefunden."
         return output
 
-    def range_per_day(self) -> list[Hotel]:
-        return [hotel for hotel in self.hotels if self.position < hotel.distance < self.position + 360]
+    def range_per_day(self, hotel: Hotel = None) -> list[Hotel]:
+        if not hotel:
+            start_distance = 0
+        else:
+            start_distance = hotel.distance
+        return filter(lambda h: start_distance < h.distance <= start_distance + 360, self.hotels)
 
-    def find_route(self) -> None:
-        non_reducible = []
-        self.hotels.sort()
-        while len(self.hotels) > 4:
-            min_hotel = None
-            for hotel in self.hotels:
-                if min_hotel is None or hotel not in non_reducible and hotel < min_hotel:
-                    min_hotel = hotel
-            if len(non_reducible) == len(self.hotels):
-                break
-            min_hotel_index = self.hotels.index(min_hotel)
-            if min_hotel_index and min_hotel_index != len(self.hotels)-1:
-                if self.hotels[min_hotel_index+1].distance - self.hotels[min_hotel_index-1].distance <= 360:
-                    del self.hotels[min_hotel_index]
-                    continue
-            elif not min_hotel_index:
-                if self.hotels[1].distance <= 360:
-                    del self.hotels[min_hotel_index]
-                    continue
-            elif min_hotel_index == len(self.hotels)-1:
-                if self.total_distance - self.hotels[-2].distance <= 360:
-                    del self.hotels[min_hotel_index]
-                    continue
-            non_reducible.append(self.hotels[min_hotel_index])
+    def find_route(self, hotel: Hotel = None, counter: int = 0, stops: list[Hotel] = []) -> Union[bool, list[Hotel]]:
+        stops = stops.copy()
+        if hotel is None:
+            daily_range = self.range_per_day()
+        else:
+            if self.min_rating and hotel.rating <= self.min_rating:
+                return False
+            daily_range = hotel.daily_range
+            stops.append(hotel)
+            if counter <= 4 and self.total_distance - 360 <= hotel.distance:
+                return stops
+            elif counter <= 4 and self.total_distance - 360 * (5 - counter) > hotel.distance:
+                return False
+        if counter < 4:
+            solution = []
+            for stop in daily_range:
+                possible_route = self.find_route(stop, counter + 1, stops)
+                if possible_route and solution:
+                    solution = max(solution, possible_route.copy(), key=lambda r: min(r))
+                    self.min_rating = min(solution).rating
+                elif possible_route and not solution:
+                    solution = possible_route.copy()
+                    self.min_rating = min(solution).rating
+            if solution:
+                return solution
+            else:
+                return False
+        else:
+            return False
 
 
 if __name__ == '__main__':
     route = Route(argv[1])
-    print(route.hotels)
+    print(route)
