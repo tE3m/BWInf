5a1a65058474ae278b80e1110eed6334d86ac794
Tarek Massini <tarek.jannis.03.tm@gmail.com>
2021-12-25 16:17:53 +0100

    Erster Lösungsansatz, löst für bisher gegebenes Beispiel

--- aufgabe3/aufgabe3.py
+++ aufgabe3/aufgabe3.py
@@ -31,14 +31,23 @@ class HexDigit:
     def __repr__(self) -> str:
         return "HexDigit({})".format(self.value)
 
+    def move_distance(self, other: int) -> int:
+        differing_segments = len([index for index in range(7) if self.representation[index] != NUMBERS[other][index]])
+        return differing_segments // 2 + differing_segments % 2
+
+    def segment_difference(self, other: int) -> int:
+        return abs(len(list(filter(lambda x: x, self.representation))) - len(list(filter(lambda x: x, NUMBERS[other]))))
+
 
 class HexNumber:
     """Eine Hexadezimalzahl"""
     digits: list[HexDigit]
+    moves: int
 
-    def __init__(self, number: str):
+    def __init__(self, number: str, moves: int):
         # Wandle den gegebenen Hexadezimal-String zu einer Liste aus Hexadezimalziffern um
         self.digits = [HexDigit(digit) for digit in number]
+        self.moves = moves
 
     def __repr__(self) -> str:
         return str([repr(digit) for digit in self.digits])
@@ -63,3 +72,35 @@ class HexNumber:
             buffer += "\n"
             index += len(segments)
         return buffer
+
+    def highest_within_moves(self) -> None:
+        for index, digit in enumerate(self.digits):
+            if digit.value == 15:
+                continue
+            for higher_digit in range(15, digit.value, -1):
+                if digit.move_distance(higher_digit) <= self.moves:
+                    if digit.segment_difference(higher_digit) == 0:
+                        self.moves -= digit.move_distance(higher_digit)
+                        self.digits[index] = HexDigit(hex(higher_digit)[2:])
+                    else:
+                        old_moves = self.moves
+                        self.moves -= digit.move_distance(higher_digit)
+                        if not self.lowest_fitting_digit(digit.segment_difference(higher_digit)):
+                            self.moves = old_moves
+            if self.moves == 0:
+                break
+
+    def lowest_fitting_digit(self, amount: int) -> bool:
+        for index, digit in enumerate(reversed(self.digits)):
+            for new_digit in range(0, 16):
+                if digit.segment_difference(new_digit) == amount and digit.move_distance(new_digit) <= self.moves:
+                    self.digits[-1-index] = HexDigit(str(new_digit))
+                    self.moves -= digit.move_distance(new_digit)
+                    return True
+        return False
+
+
+if __name__ == '__main__':
+    hexnumber = HexNumber("d24", 3)
+    hexnumber.highest_within_moves()
+    print(hexnumber)
