abd7fa498b803ca86c64c5b14821fa70f0f7820c
Tarek Massini <tarek.jannis.03.tm@gmail.com>
2021-12-25 17:55:28 +0100

    Ausgabe der Anzeige nach jedem Schritt und potenzielles Aufteilen von Segmenten

--- aufgabe3/aufgabe3.py
+++ aufgabe3/aufgabe3.py
@@ -32,11 +32,19 @@ class HexDigit:
         return "HexDigit({})".format(self.value)
 
     def move_distance(self, other: int) -> int:
-        differing_segments = len([index for index in range(7) if self.representation[index] != NUMBERS[other][index]])
-        return differing_segments // 2 + differing_segments % 2
+        amount = len(self.differing_segments(other))
+        return amount // 2 + amount % 2
 
     def segment_difference(self, other: int) -> int:
-        return abs(len(list(filter(lambda x: x, self.representation))) - len(list(filter(lambda x: x, NUMBERS[other]))))
+        return len(list(filter(lambda x: x, self.representation))) - len(list(filter(lambda x: x, NUMBERS[other])))
+
+    def differing_segments(self, other: int):
+        return [index for index in range(7) if self.representation[index] != NUMBERS[other][index]]
+
+    def digit_with_distance(self, distance: int, moves: int):
+        for new_digit in range(0, 16):
+            if self.segment_difference(new_digit) == distance and self.move_distance(new_digit) <= moves:
+                return moves-self.move_distance(new_digit), new_digit
 
 
 class HexNumber:
@@ -81,26 +89,37 @@ class HexNumber:
                 if digit.move_distance(higher_digit) <= self.moves:
                     if digit.segment_difference(higher_digit) == 0:
                         self.moves -= digit.move_distance(higher_digit)
-                        self.digits[index] = HexDigit(hex(higher_digit)[2:])
+                        to_be_removed = {index: [segment for segment in digit.differing_segments(higher_digit) if digit.representation[segment]]}
                     else:
                         old_moves = self.moves
                         self.moves -= digit.move_distance(higher_digit)
-                        if not self.lowest_fitting_digit(digit.segment_difference(higher_digit)):
+                        to_be_removed = self.additional_segments(index, digit.segment_difference(higher_digit))
+                        if to_be_removed is None:
                             self.moves = old_moves
+                            continue
+                    to_be_added = (segment for segment in digit.differing_segments(higher_digit) if NUMBERS[higher_digit][segment])
+                    for other_digit, moves in to_be_removed.items():
+                        for segment in moves:
+                            self.digits[other_digit].representation[segment] = False
+                            digit.representation[next(to_be_added)] = True
+                            print(self)
             if self.moves == 0:
                 break
 
-    def lowest_fitting_digit(self, amount: int) -> bool:
-        for index, digit in enumerate(reversed(self.digits)):
-            for new_digit in range(0, 16):
-                if digit.segment_difference(new_digit) == amount and digit.move_distance(new_digit) <= self.moves:
-                    self.digits[-1-index] = HexDigit(str(new_digit))
-                    self.moves -= digit.move_distance(new_digit)
-                    return True
-        return False
+    def additional_segments(self, start_index: int, missing: int):
+        segments = {}
+        for index, digit in enumerate(reversed(self.digits[start_index:])):
+            for amount in range(missing, 0, -1):
+                moves_and_digit = digit.digit_with_distance(amount, self.moves)
+                if moves_and_digit is not None:
+                    self.moves, new_digit = moves_and_digit
+                    missing -= amount
+                    segments[index] = [segment for segment in digit.differing_segments(new_digit) if digit.representation[segment]]
+            if not missing:
+                return segments
 
 
 if __name__ == '__main__':
     hexnumber = HexNumber("d24", 3)
-    hexnumber.highest_within_moves()
     print(hexnumber)
+    hexnumber.highest_within_moves()
