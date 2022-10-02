3840a9aa4e902fba61a595b666472ac7ad39c49d
Tarek Massini <tarek.jannis.03.tm@gmail.com>
2022-10-02 23:00:55 +0200

    Hinzufügen von Util-Methoden

--- Runde 1/aufgabe4/aufgabe4.py
+++ Runde 1/aufgabe4/aufgabe4.py
@@ -36,11 +36,43 @@ class Job:
 
 class Workshop:
     jobs: list[Job]
+    current_time: int
 
     def __init__(self, path: str):
         with open(path, "r") as file:
             lines = file.readlines()
-        self.jobs = sorted([Job(*args.strip().split(" ")) for args in lines if args != "\n"])
+        self.jobs = [Job(*args.strip().split(" ")) for args in lines if args != "\n"]
+        self.current_time = 0
+
+    @staticmethod
+    def is_working_hours(time: int) -> bool:
+        """
+        Gibt zurück, ob der Zeitpunkt `time` innerhalb der Öffnungszeiten liegt
+
+        :param time: der zu prüfende Zeitpunkt
+        :return: Wahrheitswert, ob zu diesem Zeitpunkt geöffnet ist
+        """
+        return 540 <= (time % 1440) < 1020
+
+    def next_working_hours(self, time: int | None = None) -> int:
+        """
+        Gibt den nächsten Zeitpunkt zurück, zu dem die Werkstatt geöffnet hat.
+
+        :param time: (optional) der zu prüfende Zeitpunkt. Wird kein Wert übergeben, wird die aktuelle Zeit geprüft
+        :return: der nächste Zeitpunkt, zu dem die Werkstatt geöffnet hat
+        """
+        time = self.current_time if time is None else time
+        assert time >= self.current_time
+        return time if Workshop.is_working_hours(time) else (((time // 1440) + 1) * 1440) + 540
+
+    def remaining_working_minutes(self) -> int:
+        """
+        Gibt die verbleibenden Arbeitsminuten am derzeitigen Tag zurück
+
+        :return: die verbleibenden Arbeitsminuten am derzeitigen Tag
+        """
+        assert Workshop.is_working_hours(self.current_time)
+        return 1020 - self.current_time % 1440
 
 
 if __name__ == '__main__':
